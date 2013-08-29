75caf3c2b65376f02c16e673dcc331a1033f50c4
alicem <alice.mottola@gmail.com>
2013-08-29 18:46:56 -0400

    Adds fonts to assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,46 +30,6 @@ var toggleIcon = function() {
   $(this).find('.icon-div').toggle();
 };
 
-var codexWord = function() {
-  $('#codex-porthole').html('<p class="nav-letter one-line">codex</p>');
-};
-
-var codexIcon = function() {
-  $('#codex-porthole').html('<img id="codex-icon" class="icon" src="codex.png" />');
-};
-
-var inventoryWord = function() {
-  $('#inventory-porthole').html('<p class="nav-letter three-lines">in<br>vent<br>ory</p>');
-};
-
-var inventoryIcon = function() {
-  $('#inventory-porthole').html('<img id="inventory-icon" class="icon" src="inventory.png" />');
-};
-
-var skillsWord = function() {
-  $('#skills-porthole').html('<p class="nav-letter one-line">skills</p>');
-};
-
-var skillsIcon = function() {
-  $('#skills-porthole').html('<img id="skills-icon" class="icon" src="skills.png" />');
-};
-
-var messagesWord = function() {
-  $('#messages-porthole').html('<p class="nav-letter three-lines">mes<br>sag<br>es</p>');
-};
-
-var messagesIcon = function() {
-  $('#messages-porthole').html('<img id="messages-icon" class="icon" src="messages.png" />');
-};
-
-var journalWord = function() {
-  $('#journal-porthole').html('<p class="nav-letter one-line">journal</p>');
-};
-
-var journalIcon = function() {
-  $('#journal-porthole').html('<img id="journal-icon" class="icon" src="journal.png" />');
-};
-
 var goToCodex = function() {
   $('#info-line').animate({ marginLeft: '0px' }, 750);
 };
